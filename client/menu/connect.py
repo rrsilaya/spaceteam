2bd1fd4c499816c75091ea6a2e48305ae2cb67ef
rrsilaya <rrsilaya@gmail.com>
2018-11-22 16:08:53 +0800

    Add input for username

--- client/menu/connect.py
+++ client/menu/connect.py
@@ -10,23 +10,42 @@ class Connect(tk.Frame):
     self.root = root
 
     self.chatroom = tk.StringVar()
+    self.username = tk.StringVar()
     self._isConnecting = False
 
     self._loadView()
 
   def connectChat(self, event=None):
     chatroom = self.chatroom.get()
+    username = self.username.get()
 
     self._isConnecting = True
 
     self.root.chat = Chat()
-    lobby = self.root.chat.connect(chatroom)
+    lobby = self.root.chat.connect(chatroom, username if username != '' else 'anon')
 
     self._isConnecting = False
     self.root.changeScreen(menu.Lobby)
 
   def _loadView(self):
-    inputField = tk.Entry(
+    usernameField = tk.Entry(
+      self,
+      textvariable=self.username,
+      bg='black',
+      fg='white',
+      highlightcolor='white',
+      justify=tk.CENTER,
+      state=tk.NORMAL if not self._isConnecting else 'readonly',
+      font=_getFont('title2')
+    )
+    usernameLabel = tk.Label(
+      self,
+      text='USERNAME',
+      bg='black',
+      fg='white',
+      font=_getFont('body')
+    )
+    roomIdField = tk.Entry(
       self,
       textvariable=self.chatroom,
       bg='black',
@@ -36,15 +55,24 @@ class Connect(tk.Frame):
       state=tk.NORMAL if not self._isConnecting else 'readonly',
       font=_getFont('title2')
     )
-    label = tk.Label(
+    roomLabel = tk.Label(
       self,
-      text='ENTER ROOM ID' if not self._isConnecting else 'CONNECTING...',
+      text='ROOM ID',
       bg='black',
       fg='white',
       font=_getFont('body')
     )
+    submit = tk.Button(
+      self,
+      text='CONNECT',
+      bg='black',
+      fg='white',
+      command=self.connectChat,
+      font=_getFont('title2')
+    )
 
-    inputField.bind('<Return>', self.connectChat)
-
-    label.place(x=500, y=330, anchor='center')
-    inputField.place(x=500, y=280, height=60, anchor='center')
+    usernameLabel.place(x=500, y=205, anchor='center')
+    usernameField.place(x=500, y=155, height=60, anchor='center')
+    roomLabel.place(x=500, y=315, anchor='center')
+    roomIdField.place(x=500, y=265, height=60, anchor='center')
+    submit.place(x=500, y=405, height=60, width='430', anchor='center')
