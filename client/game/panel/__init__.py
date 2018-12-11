75a96e08c2381cc30cbd7e616240480014d85057
rrsilaya <rrsilaya@gmail.com>
2018-12-11 18:32:14 +0800

    Modularize panels

---
+++ client/game/panel/__init__.py
@@ -0,0 +1,5 @@
+from game.panel.switch import Switch
+from game.panel.binarybtn import BinaryButton
+from game.panel.buttongrp import ButtonGroup
+from game.panel.vslider import VerticalSlider
+from game.panel.hslider import HorizontalSlider
